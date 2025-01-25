75b2c6fb728efaaa667b810ecee40471051d49ea
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-01-25 13:53:22 +0200

    Initial commit

---
+++ src/main/java/org/experiment/experimentalproject/ExperimentalProjectApplication.java
@@ -0,0 +1,13 @@
+package org.experiment.experimentalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperimentalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/experiment/experimentalproject/ExperimentalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.experiment.experimentalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
